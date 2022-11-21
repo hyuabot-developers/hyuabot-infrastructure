19e6cd36d9cdf584a4f41bc4050d4d43f5a51228
Jeongin Lee <jil8885@hanyang.ac.kr>
2022-11-21 17:44:04 +0900

    feat: 통학버스 기능을 스키마에 추가 (#4)

--- database/create_database.sql
+++ database/create_database.sql
@@ -7,7 +7,9 @@ create table if not exists shuttle_period_type (
 
 -- 셔틀버스 운행 노선
 create table if not exists shuttle_route (
-    route_name varchar(15) primary key
+    route_name varchar(15) primary key,
+    route_description_korean varchar(100),
+    route_description_english varchar(100)
 );
 
 -- 셔틀버스 정류장
@@ -22,6 +24,7 @@ create table if not exists shuttle_route_stop (
     route_name varchar(15) references shuttle_route(route_name),
     stop_name varchar(15) references shuttle_stop(stop_name),
     stop_order int,
+    cumulative_time int,
     constraint pk_shuttle_route_stop primary key (route_name, stop_name)
 );
 
@@ -62,6 +65,29 @@ create table if not exists shuttle_holiday(
     calendar_type varchar(15) not null
 );
 
+-- 통학버스 운행 노선
+create table if not exists commute_shuttle_route (
+    route_name varchar(15) primary key,
+    route_description_korean varchar(100),
+    route_description_english varchar(100)
+);
+
+-- 통학버스 정류장
+create table if not exists commute_shuttle_stop (
+    stop_name varchar(50) primary key,
+    description varchar(100),
+    latitude double precision,
+    longitude double precision
+);
+
+-- 통학버스 노선별 정류장 순서
+create table if not exists commute_shuttle_timetable (
+    route_name varchar(15) references commute_shuttle_route(route_name),
+    stop_name varchar(50) references commute_shuttle_stop(stop_name),
+    stop_order int,
+    departure_time timetz not null,
+    constraint pk_commute_shuttle_route_stop primary key (route_name, stop_name)
+);
 
 -- 버스 정류장
 create table if not exists bus_stop (
